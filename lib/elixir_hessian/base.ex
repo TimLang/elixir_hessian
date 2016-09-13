b6d873f5268b9459702ffee97c3f74d231d2bb6d
Tim Lang <langyong135@gmail.com>
2016-09-13 17:15:51 +0800

    add data converts method

--- lib/elixir_hessian/base.ex
+++ lib/elixir_hessian/base.ex
@@ -1,6 +1,6 @@
 
 defmodule ElixirHessian.Base do
-  
+
   defmacro __using__(_) do
     quote do
 
@@ -23,10 +23,22 @@ defmodule ElixirHessian.Base do
 
       def request method, params \\ [] do
         request_url = "#{request_host}:#{request_port}/#{package_name}"
-        ElixirHessian.Client.request(request_url, method, params)
+        convert ElixirHessian.Client.request(request_url, method, params)
       end
 
-      defoverridable Module.definitions_in(__MODULE__)
+      def convert(array) when is_tuple(hd(array)) do
+        Enum.into(array, %{})
+      end
+
+      def convert(array) when is_list(hd(array)) do
+        Enum.map(array, fn(item) -> convert(item) end)
+      end
+
+      def convert(array) do
+        array
+      end
+
+      defoverridable [request_host: 0, request_port: 0, package_name: 0]
 
     end
   end
